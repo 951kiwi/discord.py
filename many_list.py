2b446e9271805a91282771204ccde10e72978a99
951kiwi <84848077+951kiwi@users.noreply.github.com>
2021-09-11 00:39:02 +0900

    2

---
+++ many_list.py
@@ -0,0 +1,26 @@
+def color(colors):
+    if colors == "red":
+        colors = "#ff0000"
+    if colors == "green":
+        colors = "#008000"
+    if colors == "yellow":
+        colors = "#ffff00"
+    if colors == "orange":
+        colors = "#ffa500"
+    if colors == "gold":
+        colors = "#ffd700"
+    if colors == "black":
+        colors = "#000000"
+    if colors =="white" or colors=="snow":
+        colors = "#ffffff"
+    if colors == "blue":
+        colors = "#0000ff"
+    if colors == "skyblue":
+        colors = "#87ceeb"
+    if colors == "lime":
+        colors = "#00ff00"
+    if colors == "purple":
+        colors = "#800080"
+    if colors == "pink":
+        colors = "#ffc0cb"
+    return colors
